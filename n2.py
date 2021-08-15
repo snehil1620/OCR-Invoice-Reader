be3fb4039bd476d9156f1153c1b93e6a8ea34940
Snehil Verma <49275566+snehil1620@users.noreply.github.com>
2021-08-16 03:39:36 +0530

    Update n2.py

--- n2.py
+++ n2.py
@@ -2,7 +2,7 @@ import requests
 
 url = 'https://app.nanonets.com/api/v2/OCR/Model/4323c454-958f-4b55-8b06-558dbbb2dfdc/LabelFile/'
 
-data = {'file': open('D:\\Downloads\\131882.jpeg', 'rb')}
+data = {'file': open('YOUR_IMAGE_PATH', 'rb')}
 
 response = requests.post(url, auth=requests.auth.HTTPBasicAuth('mYyXQ5AFq8zGKNrDWVl-f4vmrkcl7d6_', ''), files=data)
 
